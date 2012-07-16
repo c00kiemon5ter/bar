c0eb1f8454ee094f76bb8c76452558a01eb4b298
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-07-16 15:04:56 +0300

    personal config

---
+++ config.h
@@ -0,0 +1,26 @@
+/* The height of the bar (in pixels) */
+#define BAR_HEIGHT  18
+/* The width of the bar. Set to -1 to fit screen */
+#define BAR_WIDTH   -1
+/* Offset from the left. Set to 0 to have no effect */
+#define BAR_OFFSET 1280
+/* Choose between an underline or an overline */
+#define BAR_UNDERLINE 0
+/* The thickness of the underline (in pixels). Set to 0 to disable. */
+#define BAR_UNDERLINE_HEIGHT 2
+/* Default bar position, overwritten by '-b' switch */
+#define BAR_BOTTOM 0
+/* The fonts used for the bar, comma separated. Only the first 2 will be used. */
+#define BAR_FONT       "-Misc-Stlarch-Medium-R-Normal--10-100-75-75-C-80-ISO10646-1", \
+                       "-*-terminus-medium-r-normal-*-12-*-*-*-c-*-iso10646-1"
+/* Color palette */
+#define COLOR1	0xddeedd
+#define COLOR0	0x131313
+#define COLOR2	0x222222
+#define COLOR3	0xc5000b
+#define COLOR4	0x579d1c
+#define COLOR5	0xff420e
+#define COLOR6	0x0066cc
+#define COLOR7	0x9c3885
+#define COLOR8	0x426870
+#define COLOR9	0xe2e2e5
